4546b7603c07e2167c29201d05c4ee2f4e23ca54
mohit1995singhal <57591652+mohit1995singhal@users.noreply.github.com>
2019-11-16 01:59:47 +0530

    Create jpg_to_Png.py

---
+++ jpg_to_Png.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from PIL import Image
+input_folder=sys.argv[1]
+output_folder=sys.argv[2]
+
+if not os.path.exists(output_folder):
+	os.makedirs(output_folder)
+for file_name in os.listdir(input_folder):
+	img=Image.open(f'{input_folder}{file_name}')
+	clean_name=os.path.splitext(file_name)[0]
+	img.save(f"{output_folder}{clean_name}.png",'png')
+	print("all done")
+
+
+
+
+
+
+
+
+
+
+
+
+#print(input_folder,output_folder)
+#print(os.path.exists(output_folder))
